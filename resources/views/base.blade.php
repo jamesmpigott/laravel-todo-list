da596e2698f0220f591433d81eb56bd58b5e294c
James Pigott <jmpigott96@gmail.com>
2020-02-27 21:31:52 +0000

    Include Bootstrap styles and scripts + jquery

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,8 +9,12 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+        <!-- Compiled CSS --> 
         <link rel="stylesheet" href="/assets/css/styles.css">
-        <script src="/assets/js/scripts.min.js"></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -18,5 +22,11 @@
                 @yield('body')
             </div>
         </div>
+
+        <!-- Jquery + Bootstrap JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <!-- Compiled JS -->
+        <script src="/assets/js/scripts.min.js"></script>
     </body>
 </html>
